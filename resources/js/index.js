4126c96662b4abf58836bd69fecf86973b93a97b
root <wildcats1369@gmail.com>
2024-05-31 11:43:41 +0800

    Auto Dev Deploy

--- resources/js/index.js
+++ resources/js/index.js
@@ -24,7 +24,7 @@ document.addEventListener('alpine:init', () => {
                             {
                                 id: 'custom-cta', // Unique ID for your custom block
                                 label: '{{VAR}}', // Display name for the block
-                                content: '<a href="#" class="cta-button">Click me!</a>', // Your custom HTML content
+                                content: '{{VAR}}', // Your custom HTML content
                             },
                         ],
                     },
