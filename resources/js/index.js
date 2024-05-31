1110c9a19ed88ba259b9c5b33268cf83b5d56634
root <wildcats1369@gmail.com>
2024-05-31 14:40:16 +0800

    Auto Dev Deploy

--- resources/js/index.js
+++ resources/js/index.js
@@ -7,7 +7,7 @@ document.addEventListener('alpine:init', () => {
             tools: tools,
             async init() {
                 let [content1] = await Promise.all([
-                    fetch('/js/wildcats1369/filament-grapesjs-v3/test.html').then(response => response.text())
+                    fetch('/test.html').then(response => response.text())
                 ]);
                 console.log(content1);
                 this.instance = grapesjs.init({
